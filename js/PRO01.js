ab78b2e22f9d4bdcda07042bb9d270ad250903e6
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-02-28 20:29:35 +0100

    web

---
+++ js/PRO01.js
@@ -0,0 +1,38 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled'); // Agregar la clase cuando se hace scroll hacia abajo
+    } else {
+        navbar.classList.remove('scrolled'); // Quitar la clase cuando se vuelve a la parte superior
+    }
+});
